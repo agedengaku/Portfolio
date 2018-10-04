39ba6a64140f79a46e0756e129f53ae1e8410b9f
aburiengawa <jmotaylor2@gmail.com>
2018-10-05 00:00:00 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,296 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>JM Taylor</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-9ralMzdK1QYsk4yBY680hmsb4/hJ98xK3w0TIaJ3ll4POWpWUYaA2bRjGGujGT8w" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+  <link rel="stylesheet" href="resources/css/style.css">
+</head>
+
+<body>
+
+   <nav class="navbar sticky-top navbar-toggleable-md navbar-inverse navbar-color">
+    
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <!--<span class="navbar-toggler-icon"></span>-->
+      <div id="nav-icon0">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+       </div>
+    </button>
+
+    <a class="navbar-brand" href="#">
+      jmtaylor<span class="animated infinite flash">_</span> 
+    </a>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="nav navbar-nav mr-auto"></ul>
+      <ul class="nav navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link intro-link" data-toggle="pill" href="#intro-block"><p>About</p></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link portfolio-link" data-toggle="pill" href="#portfolio-block"><p>Portfolio</p></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link contact-link" data-toggle="pill" href="#contact-block"><p>Contact</p></a>
+        </li>
+      </ul>
+    </div> <!-- #navbarNav -->
+
+</nav> <!-- END OF NAVBAR -->
+
+<!-- INTRO BLOCK STARTS HERE -->
+<div class="container">
+    <div class="row justify-content-md-center"> <!-- .row.justify-content-md-center 1 -->
+    
+        <div id="intro-block" class="content-block col-12 col-lg-auto">
+          <div class="row">
+            <div id="intro-block-body" class="col-md-8 text-right">Web design & development is a passion of mine. There isn't a day I'm not either working on personal projects or continuing to learn web technologies. In the future, I plan to move into mobile apps and software for smart devices.</div>
+            <div class="col-md-4"><img id="circle-pic" src="https://en.gravatar.com/userimage/27569311/49b5d4c8c1114783cc08646adb285384.jpg?size=400" alt="" class="img-fluid" /></div>
+          </div> <!-- .row -->
+        </div> <!-- #intro-block -->
+    
+    </div> <!-- .row.justify-content-md-center -->
+    
+    <!-- PORTFOLIO BLOCK STARTS HERE -->
+    
+    <div class="row justify-content-md-center"> <!-- .row.justify-content-md-center 2 -->
+    
+        <div id="portfolio-block" class="content-block col-12 col-lg-auto">
+          <h2 class="text-center">Portfolio</h2>
+          
+          <!-- <custom gradient hr"> -->
+          <div class="gradient-border identity-border"></div>
+          
+          <div class="row">
+            
+            <div class="col-md-6 portfolio-pic-container" data-toggle="popover" data-placement="bottom" title="Tic Tac Toe - Street Fighter 2 version<span class='close-popover'><i class='fal fa-times fa-lg'></i></span>" data-content="Integrated minimax to create an unwinnable difficulty mode (Hard mode). Animations created using a combination of sprite maps and animated GIFs, complete with intro video, title screen, character select, etc. to simulate the Street Fighter 2 game experience. Also uses Web Audio API to create seamless looping BGM.<br /><br />Works best on Chrome and Firefox">
+                <div class="portfolio-pic">
+                <span class="pic-title">Tic Tac Toe</span>
+              <a href="http://streetfighter2ttt.com" target="_blank">
+                <img src="/resources/img/tictactoe-web.jpg" class="img-fluid" alt="Street Fighter 2 - Tic Tac Toe">
+              </a>
+              </div>
+            </div>
+            
+            <div class="col-md-6 portfolio-pic-container" data-toggle="popover" data-placement="bottom" title="Simon Game - Street Fighter 2 version<span class='close-popover'><i class='fal fa-times fa-lg'></i></span>" data-content="Spiced up the original Simon game by using Street Fighter 2 themes, music, sfx, and some animated feedback.<br /><br />Works best on Chrome and Firefox">  
+              <a href="https://codepen.io/agedengaku/full/gjLOra/" target="_blank">
+              <img src="/resources/img/simon-web.jpg" class="img-fluid portfolio-pic effectfront" alt="Street Fighter 2 - Simon Game">
+              </a>
+            </div>
+            
+          </div> <!-- .row -->        
+          
+          <div class="row">
+            
+            <div class="col-md-6 portfolio-pic-container" data-toggle="popover" data-trigger="hover" data-placement="bottom" data-html="true" title="Javascript Calculator<span class='close-popover'><i class='fal fa-times fa-lg'></i></span>" data-content="Improved on the original task of creating a Javascript calcultor by increasing decimal precision, making the results always similar to what you would get from a real calculator. Precision is achieved using a Decimal type by integrating decimal.js.">
+              <a href="https://codepen.io/agedengaku/full/NYPyWy/" target="_blank">
+                <img src="/resources/img/calculator-web.jpg" class="img-fluid portfolio-pic effectfront" alt="Javascript Calcultar">
+              </a>
+            </div>
+            
+            <div class="col-md-6 portfolio-pic-container" data-toggle="popover" data-trigger="hover" data-placement="bottom" data-html="true" title="Pomodoro Clock<span class='close-popover'><i class='fal fa-times fa-lg'></i></span>" data-content="Goes beyond the assignment of building a productivity clock by adding animations and original recorded sound clips to guide the user on the current state of the clock.">  
+              <a href="https://codepen.io/agedengaku/full/bvZbGx/" target="_blank">
+              <img src="/resources/img/pomodoro-web.jpg" class="img-fluid portfolio-pic effectfront" alt="Pomodoro Clock">
+              </a>
+            </div>
+            
+          </div> <!-- .row -->      
+          
+          <div class="row">
+            
+            <div class="col-md-6 portfolio-pic-container" data-toggle="popover" data-trigger="hover" data-placement="bottom" data-html="true" title="Japanese Lessons Tokyo<span class='close-popover'><i class='fal fa-times fa-lg'></i></span>" data-content="Built a website that helped a Japanese language teacher from almost going out of business, and allowed him to quit his part-time jobs to focus solely on teaching.">
+              <a href="https://japaneselessonstokyo.com" target="_blank">
+                <img src="/resources/img/jlt-web.jpg" class="img-fluid portfolio-pic effectfront" alt="Japanese Lessons Tokyo website picture">
+              </a>
+            </div>
+            
+            <div class="col-md-6 portfolio-pic-container" data-toggle="popover" data-trigger="hover" data-placement="bottom" data-html="true" title="Redesign of outdated website<span class='close-popover'><i class='fal fa-times fa-lg'></i></span>" data-content="An exercises in redesigning an old and outdated website, including a new modern logo. Done on Wordpress, but the theme was coded completely from scratch using PHP. <a href='http://www.xpstokyo.com' target='_blank'>Click here to see the original website.</a>">  
+              <a href="/xps" target="_blank">
+              <img src="/resources/img/xpstokyo-web.jpg" class="img-fluid portfolio-pic effectfront" alt="XPS Tokyo website picture">
+              </a>
+            </div>
+            
+          </div> <!-- .row -->
+          
+          <div class="row">
+            
+            <div class="col-md-6 portfolio-pic-container" data-toggle="popover" data-trigger="hover" data-placement="bottom" data-html="true" title="Color Game<span class='close-popover'><i class='fal fa-times fa-lg'></i></span>" data-content="A game for web designers testing their knowledge of RGB colors. This was an exercise from an online web development bootcamp, and was my first Javascript project.">  
+              <a href="/hello/colorgame" target="_blank">
+              <img src="/resources/img/color-web.jpg" class="img-fluid portfolio-pic effectfront" alt="Color Game web app picture">
+              </a>
+            </div>
+            
+            <div class="col-md-6 portfolio-pic-container" data-toggle="popover" data-trigger="hover" data-placement="bottom" data-html="true" title="Duterte Ipsum<span class='close-popover'><i class='fal fa-times fa-lg'></i></span>" data-content="Inspired by <a href='https://trumpipsum.net/' target='_blank'>Trump Ipsum</a>, this Lorem Ipsum generator uses some of President Duterte's ridiculous quotes intersperesed with tech and web design related words.<br />Coded with PHP.">  
+              <a href="http://www.duterteipsum.com" target="_blank">
+              <img src="/resources/img/duterte-web.jpg" class="img-fluid portfolio-pic effectfront" alt="Duterte Ipsum website picture">
+              </a>
+            </div>
+            
+          </div> <!-- .row -->
+          
+        <div class="row">
+            
+            <div class="col-md-6 portfolio-pic-container" data-toggle="popover" data-trigger="hover" data-placement="bottom" data-html="true" title="Design Quotes<span class='close-popover'><i class='fal fa-times fa-lg'></i></span>" data-content="A certification project wherein a quotes-generating API and Twitter API are integrated to build a random quote machine with Twitter functionality.">  
+              <a href="/designquotes" target="_blank">
+              <img src="/resources/img/quotes-web.jpg" class="img-fluid portfolio-pic effectfront" alt="Design Quotes website picture">
+              </a>
+            </div>
+            
+            <div class="col-md-6 portfolio-pic-container">  
+              <a href="/wikiviewer" target="_blank">
+              <img src="/resources/img/wiki-web.jpg" class="img-fluid portfolio-pic effectfront" alt="Wikiviewer website picture">
+              </a>
+            </div>
+            
+          </div> <!-- .row -->
+          
+        </div> <!-- portfolio-block -->
+    
+    </div> <!-- .row.justify-content-md-center 2 -->
+    
+    <!-- CONTACT FORM STARTS HERE -->
+    
+    <div class="row justify-content-md-center"> <!-- .row.justify-content-md-center 3 -->
+    
+      <div id="contact-block" class="content-block col-12 col-lg-auto">
+        <h2 class="text-center">Contact Me</h2>
+    
+        <!-- <custom gradient hr --> 
+        <div class="gradient-border-white identity-border"></div>
+        
+        <div class="row">
+          <div class="col-md-6">
+            
+            <form method="get" action="/thisdoesntworkyet.html">
+              
+              <div class="form-group">
+                <div class="label-div">
+                  <label class="js-name-label" for="name">Name</label>
+                </div>  
+                <input type="text" class="form-control js-name-input" id="name" placeholder="Name">
+              </div>
+              
+              <div class="form-group">
+                <div class="label-div">
+                  <label class="js-email-label" for="email">Email</label>
+                </div>
+                <input type="text" class="form-control js-email-input" id="email" placeholder="Email">
+              </div>
+              
+              <div class="form-group">
+                <div class="label-div">
+                  <label class="js-phone-label" for="phone">Phone</label>
+                </div>
+                <input type="text" class="form-control  js-phone-input" id="phone" placeholder="Phone">
+              </div>
+              
+              <div class="form-group">
+                <div class="label-div">
+                  <label class="js-message-label" for="message">Message</label>
+                </div>
+                <textarea id="contact-textarea" class="form-control js-message-input" id="message" rows="5" placeholder="Message"></textarea>
+              </div>
+              
+              <button type="submit" class="btn submit-button">Submit</button>
+              
+            </form>
+            
+          </div> <!-- .col-md-6 -->
+          <div class="col-md-6" id="contact-text">
+            If you'd like to get in touch to talk about web design and development, or perhaps you need advice for your lovelife, feel free to contact me using the form. I'll get back to you within 24 hours!
+          </div>
+          
+        </div> <!-- .row -->
+        
+      </div> <!-- #contactBlock -->
+    
+    </div> <!-- .row.justify-content-md-center 3 -->
+</div> <!-- .container -->
+<footer id="footer-1" class="footer">
+  <div class="container">
+    
+    <div class="row">
+      
+      <div class="col-sm-6">
+        
+        <div class="footer-h2">
+          <h2 class="text-center">ABOUT THIS PAGE</h2>
+        </div>
+        
+        <p class="text-center">Made with &nbsp;
+          <a href="https://codepen.io" target="_blank">
+            <img src="http://cdn.onlinewebfonts.com/svg/img_336066.svg" alt="CodePen logo" /></a>&nbsp;&nbsp;
+          <a href="http://v4-alpha.getbootstrap.com/" target="_blank">
+            <img src="https://v4-alpha.getbootstrap.com/apple-touch-icon.png" alt="Bootstrap-4 logo" />
+          </a>&nbsp;
+          <a href="http://jquery.com/" target="_blank">
+            <img src="https://uploads.toptal.io/blog/category/logo/40/jquery.png" alt="jQuery logo" />
+          </a>&nbsp;
+          <a href="http://fontawesome.io/" target="_blank">
+            <i class="fa fa-font-awesome" aria-hidden="true"></i>
+          </a></p>
+        
+      </div> <!-- .col-sm-6 -->
+      
+      <div class="col-sm-6">
+        
+        <div class="footer-h2">
+          <h2 class="text-center">AROUND THE WEB</h2>
+        </div>
+        
+        <div class="text-center">
+          <a href="https://twitter.com/jmotaylor" target="_blank">
+            <span class="fal-stack fa-lg">
+              <i class="fal fa-circle-o fa-stack-2x" aria-hidden="true"></i>
+              <i class="fal fa-twitter fa-stack-1x" aria-hidden="true"></i>
+            </span>
+          </a>
+          <a href="https://github.com/agedengaku" target="_blank">
+            <span class="fal-stack fa-lg">
+              <i class="fal fa-circle-o fa-stack-2x" aria-hidden="true"></i>
+              <i class="fal fa-github fa-stack-1x" aria-hidden="true"></i>
+            </span>
+          </a>
+          <a href="https://www.instagram.com/taylor.jm/" target="=blank">
+            <span class="fal-stack fa-lg">
+              <i class="fal fa-circle-o fa-stack-2x" aria-hidden="true"></i>
+              <i class="fal fa-instagram fa-stack-1x" aria-hidden="true"></i>
+            </span>
+          </a>
+          <a href="https://www.facebook.com/therealjmtaylor" target="_blank">
+            <span class="fal-stack fa-lg">
+              <i class="fal fa-circle-o fa-stack-2x" aria-hidden="true"></i>
+              <i class="fal fa-facebook fa-stack-1x" aria-hidden="true"></i>
+            </span>
+          </a>  
+        </div> <!-- .text-center -->
+        
+      </div> <!-- .col-sm-6 -->
+      
+    </div> <!-- .row -->
+    
+  </div> <!-- .container -->
+</footer>
+
+<footer id="footer-2" class="footer">
+  <div class="container">
+    <div class="text-center">JM Taylor &#169; <?php echo date('Y'); ?> - All Rights Reserved</div>
+  </div>
+</footer>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
+<script src="resources/js/script.js"></script>
+</body>
+</html>
---
+++ resources/js/script.js
@@ -0,0 +1,89 @@
+$(document).ready(function(){
+  
+  //navbar link navigation
+  $('.intro-link').click(function(){ 
+    $('html, body').animate({scrollTop: $('#intro-block').offset().top - 120}, 1000);
+    });
+  
+  $('.portfolio-link').click(function(){ 
+    $('html, body').animate({scrollTop: $('#portfolio-block').offset().top - 120}, 1000);
+  });
+  
+  $('.contact-link').click(function(){ 
+    $('html, body').animate({scrollTop: $('#contact-block').offset().top - 120}, 1000);
+  });
+
+  //label animation
+  $('.js-name-input').keypress(function(direction){
+    $('.js-name-label').css('display', 'block')
+    $('.js-name-label').addClass('animated fadeInUp');
+  }); 
+
+  $('.js-email-input').keypress(function(direction){
+    $('.js-email-label').css('display', 'block')
+    $('.js-email-label').addClass('animated fadeInUp');
+  });
+  
+  $('.js-phone-input').keypress(function(direction){
+    $('.js-phone-label').css('display', 'block')
+    $('.js-phone-label').addClass('animated fadeInUp');
+  });
+  
+  $('.js-message-input').keypress(function(direction){
+    $('.js-message-label').css('display', 'block')
+    $('.js-message-label').addClass('animated fadeInUp');
+  });  
+
+  //Submit button animation
+  $('#contact-block').waypoint(function(direction){
+    $('.submit-button').addClass('animated pulse');
+  }, {
+    offset: '20%'
+  });
+//SOURCE: https://embed.plnkr.co/plunk/HLqrJ6  
+  $('[data-toggle="popover"]').popover({trigger: "manual", html: true, animation: false})
+    .on("mouseenter", function (){
+      let that = this;
+      $(this).popover("show");
+      $(".popover").on("mouseleave", function (){
+        $(that).popover('hide');
+      });
+    }).on("mouseleave", function(){
+      let that = this;
+      setTimeout(function(){
+        if (!$(".popover:hover").length) {
+          $(that).popover("hide");
+        }
+    },200);
+}); 
+//END SOURCE  
+  $(document).on('click touchstart', '.close-popover', function() {
+    $('.popover').popover('hide');
+  });
+//SOURCE: https://stackoverflow.com/questions/7069458/prevent-touchstart-when-swiping  
+    let touchmoved;
+    $('body div:not(.popover)').on('touchend', function(){
+      if(!touchmoved){
+        $('.popover').popover('hide');
+      }
+    }).on('touchmove', function(){
+          touchmoved = true;    
+    }).on('touchstart', function(){
+          touchmoved = false;
+    }); 
+//END SOURCE
+
+//SOURCE: https://bootsnipp.com/snippets/PEkG1
+$('.navbar-toggler').click(function(){
+       // alert("open");
+    if (!$('.navbar-collpase').hasClass('show')) {
+        $('#nav-icon0').addClass('open');    
+    }
+	if ($('.navbar-collapse').hasClass('show')) {
+	    $('#nav-icon0').removeClass('open');    
+	}
+});
+//END SOURCE
+
+
+});
